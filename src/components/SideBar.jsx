df2b35b4bfcc40f3f24444a2f6e12508ca147066
Kyle Olson <kaollie74@gmail.com>
2022-08-29 19:40:44 -0500

    implemented Feed and SideBar UI, no functionality

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,43 @@
+import { Stack } from "@mui/material";
+
+import { categories } from "../utils/constants";
+const selectedCategory = "New";
+const SideBar = () => (
+    <Stack
+        direction="row"
+        sx={{
+            overflowY: "auto",
+            height: { sx: "auto", md: "95%" },
+            flexDirection: { md: "column" },
+        }}
+    >
+        {categories.map(({ icon, name }) => (
+            <button
+                className="category-btn"
+                style={{
+                    background: name === selectedCategory && "#FC1503",
+                    color: "white",
+                }}
+                key={name}
+            >
+                <span
+                    style={{
+                        color: name === selectedCategory ? "white" : "red",
+                        marginRight: "15px",
+                    }}
+                >
+                    {icon}
+                </span>
+                <span
+                    style={{
+                        opacity: name === selectedCategory ? "1" : "0.8",
+                    }}
+                >
+                    {name}
+                </span>
+            </button>
+        ))}
+    </Stack>
+);
+
+export default SideBar;
